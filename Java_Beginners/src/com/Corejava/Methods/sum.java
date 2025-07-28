fef6e0aa458b7f3b5f7426005cc81fb16181e9d1
Shibanand896 <130650031+Shibanand896@users.noreply.github.com>
2025-07-28 12:59:32 +0530

    Add sum.java with add method example
    Introduces a new class demonstrating a static add method that sums two integers and prints the result.

---
+++ Java_Beginners/src/com/Corejava/Methods/sum.java
@@ -0,0 +1,11 @@
+package com.Corejava.Methods;
+
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(add(20,56));
+
+    }
+    static int add(int n, int m){
+        return n+m;
+    }
+}
